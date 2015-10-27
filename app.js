9f7633f55910d708cb8415ae363958ca57b77005
NckHmml <NckHmml@users.noreply.github.com>
2015-10-27 15:19:24 +0900

    Added solution in NodeJS

---
+++ app.js
@@ -0,0 +1,117 @@
+﻿var restify = require("restify"),
+    secret = "challenge-database-api",
+    server = restify.createServer(),
+    endpoint = "/api/challenge/:itemId",
+    db = require("./database.js");
+
+server.use(restify.bodyParser());
+
+server.put(endpoint, put);
+server.post(endpoint, post);
+server.del(endpoint, del);
+server.get(endpoint, restify.queryParser(), gets);
+
+server.listen(9000);
+
+function put(req, res, next) {
+    var hmac = require('crypto').createHmac("sha256", secret),
+        data = req.body,
+        digest = hmac.update("/api/challenge/;PUT").digest("hex"),
+        itemId = req.params.itemId;
+    
+    if (digest != data.digest) {
+        res.send(401);
+        return next();
+    }
+    
+    db.updateChallenge(itemId, data.challenge, function (err) {
+        if (err) {
+            res.send(200, {
+                "challengeId": itemId,
+                "result": false,
+                "reason": err.toString()
+            });
+        } else {
+            res.send(200, {
+                "challengeId": itemId,
+                "result": true
+            });
+        }
+        
+        return next();
+    });
+}
+
+function post(req, res, next) {
+    var hmac = require('crypto').createHmac("sha256", secret),
+        data = req.body,
+        digest = hmac.update("/api/challenge/;POST").digest("hex");
+    
+    if (digest != data.digest) {
+        res.send(401);
+        return next();
+    }
+    
+    db.insertChallenge(data.challenge, function (lastId) {
+        if (lastId) {
+            res.send(200, {
+                "challengeId": lastId,
+                "result": true
+            });
+        } else {
+            res.send(200, {
+                "result": false,
+                "reason": "Something went wrong"
+            });
+        }
+        return next();
+    });
+}
+
+function del(req, res, next) {
+    var hmac = require('crypto').createHmac("sha256", secret),
+        data = req.body,
+        digest = hmac.update("/api/challenge/;DELETE").digest("hex"),
+        itemId = req.params.itemId;
+        
+    if (digest != data.digest) {
+        res.send(401);
+        return next();
+    }
+    
+    db.deleteChallenge(itemId, function () {        
+        res.send(200, {
+            "result": true
+        });
+        
+        return next();
+    })
+}
+
+function gets(req, res, next) {
+    var hmac = require('crypto').createHmac("sha256", secret),
+        data = req.query,
+        digest = hmac.update("/api/challenge/;GET").digest("hex"),
+        itemId = req.params.itemId;
+    
+    if (digest != data.digest) {
+        res.send(401);
+        return next();
+    }
+    
+    db.getChallenge(itemId, function (challenge) {
+        if (challenge) {
+            res.send(200, {
+                "challenge": challenge,
+                "result": true
+            });
+        } else {
+            res.send(200, {
+                "result": false,
+                "reason": "No such challenge"
+            });
+        }
+        
+        return next();
+    })
+}
---
+++ database.js
@@ -0,0 +1,34 @@
+﻿var db = new (require("sqlite3")).Database("C:/Users/Nick/Documents/Visual Studio 2013/Projects/challenge-database-api/challenge-database-api/sql/database.db");
+
+exports.insertChallenge = function (challenge, callback) {
+    db.run("INSERT INTO challenges (name, description, date) VALUES ($name, $description, $date)", {
+        $name: challenge.name,
+        $description: challenge.description,
+        $date: challenge.date
+    }, function (err) {
+        callback(this.lastID);
+    });
+};
+
+exports.updateChallenge = function (challengeId, challenge, callback) {
+    db.run("UPDATE challenges SET name = $name, description = $description, date = $date WHERE id = $id", {
+        $name: challenge.name,
+        $description: challenge.description,
+        $date: challenge.date,
+        $id: challengeId
+    }, callback);
+};
+
+exports.getChallenge = function (challengeId, callback) {
+    db.get("SELECT * FROM challenges WHERE id = $id", {
+        $id: challengeId
+    }, function (err, row) {
+        callback(row);
+    });
+}
+
+exports.deleteChallenge = function (challengeId, callback) {
+    db.run("DELETE FROM challenges WHERE id = $id", {
+        $id: challengeId
+    }, callback);
+}
